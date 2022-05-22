1512fc8e35169d3a69ac615fde33f945f2c99fce
engineeralok <engineeralok07@gmail.com>
2022-05-22 15:51:03 +0530

    Step-2

--- lib/app/sign_in/sign_in.dart
+++ lib/app/sign_in/sign_in.dart
@@ -12,7 +12,46 @@ class SignInPage extends StatelessWidget {
         ),
         elevation: 2.0,
       ),
-      body: Container(),
+      body: _buildContent(),
     );
   }
+}
+
+Widget _buildContent() {
+  return Padding(
+    padding: const EdgeInsets.all(20.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          color: Colors.orange,
+          child: const SizedBox(
+            height: 177,
+            width: 100,
+          ),
+        ),
+        const SizedBox(
+          height: 10.0,
+        ),
+        Container(
+          color: Colors.red,
+          child: const SizedBox(
+            height: 177,
+            width: 100,
+          ),
+        ),
+        const SizedBox(
+          height: 10.0,
+        ),
+        Container(
+          color: Colors.blue,
+          child: const SizedBox(
+            height: 177,
+            width: 100,
+          ),
+        ),
+      ],
+    ),
+  );
 }
