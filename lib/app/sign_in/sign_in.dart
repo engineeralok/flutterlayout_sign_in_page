0771b0ac19b499edfd8ec578cf125d3903ce2091
engineeralok <engineeralok07@gmail.com>
2022-05-24 12:29:16 +0530

    Step-3

--- lib/app/sign_in/sign_in.dart
+++ lib/app/sign_in/sign_in.dart
@@ -13,6 +13,7 @@ class SignInPage extends StatelessWidget {
         elevation: 2.0,
       ),
       body: _buildContent(),
+      backgroundColor: Colors.grey[200],
     );
   }
 }
@@ -24,32 +25,38 @@ Widget _buildContent() {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Container(
-          color: Colors.orange,
-          child: const SizedBox(
-            height: 177,
-            width: 100,
+        const Text(
+          'Sign In',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.w600,
           ),
         ),
         const SizedBox(
           height: 10.0,
         ),
-        Container(
-          color: Colors.red,
-          child: const SizedBox(
-            height: 177,
-            width: 100,
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: const Color.fromARGB(255, 255, 255, 255),
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(20),
+              ),
+            ),
+            minimumSize: const Size(0, 50),
           ),
-        ),
-        const SizedBox(
-          height: 10.0,
-        ),
-        Container(
-          color: Colors.blue,
-          child: const SizedBox(
-            height: 177,
-            width: 100,
+          child: const Text(
+            'Sign in with Google',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Color.fromARGB(255, 28, 19, 19),
+              fontSize: 15,
+            ),
           ),
+          onPressed: () {
+            print('Buttton Pressed');
+          },
         ),
       ],
     ),
